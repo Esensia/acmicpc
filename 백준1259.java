b04a41b75767e3458aa51ba8c7d5194295d2646e
Esensia <41403898+Esensia@users.noreply.github.com>
2019-04-18 18:58:14 +0900

    Update 백준1259.java

--- 백준1259.java
+++ 백준1259.java
@@ -1,5 +1,38 @@
-package start;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.*;
 
-public class 백준1259 {
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			String str = sc.nextLine();
+			boolean answer = true;
+			if(str.equals("0"))
+				break;
+			for(int i=0;i<str.length()/2;i++) {
+				if(str.charAt(i)==str.charAt(str.length()-i-1)) {
+					continue;
+				}
+				else {
+					answer = false;
+					break;
+				}
+			}
+			if(answer) {
+				System.out.println("yes");
+			}
+			else {
+				System.out.println("no");
+			}
+		}
+	}
 }
